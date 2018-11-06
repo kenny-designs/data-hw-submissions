cefaca69637ce814805dbabe0ce165f355b76e1c
Alexander Aguilar <alexanderman2004@yahoo.com>
2018-11-05 21:34:34 -0500

    Refactored code and reworked how we form wordcloud

--- assignment3/cloud-producer/src/main/java/com/kennydesigns/app/App.java
+++ assignment3/cloud-producer/src/main/java/com/kennydesigns/app/App.java
@@ -1,88 +1,82 @@
 package com.kennydesigns.app;
 
-// Reading in files with lyrics
-import java.util.Scanner;
-import java.io.FileReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.FileNotFoundException;
-
+// Used for finding the frequency of words from a given file
 import com.kennydesigns.app.WordCounter;
 
-// Data structures
+// Import needed classes for kumo to create the wordcloud
+import java.io.IOException;
+import java.io.File;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Map.Entry;
-
-// import needed classes for kumo word cloud
-import com.kennycason.kumo.nlp.FrequencyAnalyzer;
+import com.kennycason.kumo.nlp.FrequencyFileLoader;
 import com.kennycason.kumo.WordFrequency;
 import com.kennycason.kumo.WordCloud;
 import com.kennycason.kumo.CollisionMode;
-import com.kennycason.kumo.bg.RectangleBackground;
-import com.kennycason.kumo.bg.CircleBackground;
 import com.kennycason.kumo.bg.PixelBoundryBackground;
 import com.kennycason.kumo.palette.ColorPalette;
 import com.kennycason.kumo.font.scale.LinearFontScalar;
-import com.kennycason.kumo.font.scale.SqrtFontScalar;
 import java.awt.Dimension;
 import java.awt.Color;
 
 /**
- * Entry point into our application
+ * Entry point to program that reads in the lyrics.txt file and creates a word
+ * word cloud based on it.
  */
 public class App {
-  private static final String LYRICS = "./input/lyrics.txt";
-  private static final String OUTPUT = "./output/wordcloud.png";
-  private static final String FILTER = "[(),]";
-
-  public static void main( String[] args ) throws IOException {
-    // read in the lyrics and count the frequency with which they appear
-    WordCounter wc = readLyrics();
-
-    // create a WordFrequency List for generating the WordCloud
-    List<WordFrequency> wordFrequencies = new ArrayList<WordFrequency>();
-
-    // populate the WordFrequency List based on our WordCounter
-    for (Entry<String, Integer> entry : wc.getEntrySet()) {
-      wordFrequencies.add(new WordFrequency(entry.getKey(), entry.getValue()));
-    }
-
-    // create the WordCloud
-    final Dimension dimension = new Dimension(1000, 1000);
-    final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
-    wordCloud.setPadding(2);
-    wordCloud.setBackground(new PixelBoundryBackground("./input/windmill.png"));
-    wordCloud.setColorPalette(new ColorPalette(new Color(0x934334), new Color(0x2F472D), new Color(0x75B84F), new Color(0xFFFFFF)));
-    wordCloud.setFontScalar(new LinearFontScalar(20, 60));
-    wordCloud.build(wordFrequencies);
-    wordCloud.writeToFile(OUTPUT);
-  }
+  private static final String OUTPUT_IMG = "./output/wordcloud.png",  // location to save wordcloud
+                              OUTPUT_TXT = "./output/output.txt";     // location to save output.txt
 
   /**
-   * Read the lyrics from LYRICS and count their frequency with WordCounter
+   * Entry point to program
    */
-  public static WordCounter readLyrics() {
-    // utilized to count lyrics via a HashMap
-    WordCounter wc = new WordCounter();
+  public static void main(String[] args) {
+    // exit program if user gave too many or too few args
+    if (args.length < 2) {
+      System.out.println("Too few arguments. Please supply path to lyrics and path to image");
+      System.exit(0);
+    }
+    else if (args.length > 3) {
+      System.out.println("Too many arguments. At most, supply a path to lyrics, path to an image, and a regular expression");
+      System.exit(0);
+    }
 
+    // retrieve location of the lyrics file, the image file, and optional regular expression from the user
+    // filter defaults to an empty String
+    String lyrics =   args[0],
+           inputImg = args[1],
+           filter =   args.length == 3 ? args[2] : "";
+
+    // read in the lyrics and count the frequency with which they appear
+    WordCounter wc = new WordCounter(lyrics, filter);
+
+    // export the lyrics to a .txt file in the form of 'frequency: word'
+    wc.exportWords(OUTPUT_TXT);
+
+    // create the WordCloud
+    System.out.println("Now creating wordcloud...");
     try {
-      // read in lyrics
-      Scanner sc = new Scanner(new File(LYRICS));
+      // load in the 'frequency: word' word pair based on the OUTPUT_TXT file
+      final FrequencyFileLoader frequencyFileLoader = new FrequencyFileLoader();
+      final List<WordFrequency> wordFrequencies = frequencyFileLoader.load(new File(OUTPUT_TXT));
 
-      // log each word in lyric and increment its count in WordCounter
-      while (sc.hasNext()) {
-        // remove any non-word elements
-        wc.incrementCounter(sc.next().toLowerCase().replaceAll(FILTER, ""));
-      }
+      // create the dimensions of the new wordcloud and set its CollisionMode
+      final Dimension dimension = new Dimension(1000, 1000);
+      final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
 
-      sc.close();
+      // with the word cloud created, set some basic properties
+      wordCloud.setPadding(2);
+      wordCloud.setBackground(new PixelBoundryBackground(inputImg));
+      wordCloud.setColorPalette(new ColorPalette(new Color(0x934334), new Color(0x2F472D), new Color(0x75B84F), new Color(0xFFFFFF)));
+      wordCloud.setFontScalar(new LinearFontScalar(20, 60));
+      wordCloud.build(wordFrequencies);
+
+      // write the final cloud to the OUTPUT_IMG file
+      wordCloud.writeToFile(OUTPUT_IMG);
+      System.out.println("Success!");
     }
-    catch (FileNotFoundException e) {
+    catch (IOException e) {
+      // something went wrong, report to user
       e.printStackTrace();
+      System.out.println("Failed to create wordcloud.");
     }
-
-    // return the final WordCounter
-    return wc;
   }
 }
--- assignment3/cloud-producer/src/main/java/com/kennydesigns/app/WordCounter.java
+++ assignment3/cloud-producer/src/main/java/com/kennydesigns/app/WordCounter.java
@@ -3,31 +3,128 @@ package com.kennydesigns.app;
 // Used for tracking word frequency
 import java.util.HashMap;
 
-// Used for return the entry set
-import java.util.Map.Entry;
-import java.util.Set;
+// Used for reading and writing text files containing the words to count
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+// Sorting the HashMap from the most frequent word to the least
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
- * Makes use of HashMap to track the frequency certain lyrics appear
+ * Tracks the frequency at which words appear in a given file
  */
 public class WordCounter {
+  // HashMap to check word frequency
   private HashMap<String, Integer> frequency = new HashMap<>();
 
   /**
-   * Increment the value associated with the given key
+   * Constructs a new WordCounter based on given file
+   *
+   * @param file The file we wish to find the frequency of
    */
-  public void incrementCounter(String key) {
+  public WordCounter (String file) {
+    // populate HashMap frequency based on given file and no filter
+    this(file, "");
+  }
+
+  /**
+   * Constructs a new WordCounter based on given file and filter
+   *
+   * @param file The file we wish to find the frequency of
+   * @param filter Regular expression used to filter each word of unwanted characters
+   */
+  public WordCounter (String file, String filter) {
+    // populate HashMap frequency based on given file and filter
+    readFileFrequency(file, filter);
+  }
+
+  /**
+   * Increment the value associated with the given key
+   *
+   * @param key The key we wish to increment the value of in the HashMap frequency
+   */
+  private void incrementCounter(String key) {
     // if the key exists, store its value. If not, set to 0
     Integer val = frequency.containsKey(key) ? frequency.get(key) : 0;
 
-    // update the key/value pair in the HashMap
+    // increment the value associated with the key by 1
     frequency.put(key, val + 1);
   }
 
   /**
-   * Used for returning the entry set
+   * Reads in the text from the given file and produces a HashMap based on how
+   * frequent each word is
+   *
+   * @param file The file we wish to find the frequency of
+   * @param filter Regular expression with characters we want removed from all words
    */
-  public Set<Entry<String, Integer>> getEntrySet() {
-    return frequency.entrySet();
+  private void readFileFrequency(String file, String filter) {
+    try {
+      // read in the given file
+      Scanner sc = new Scanner(new File(file));
+
+      // log each word from the file and find its frequency
+      while (sc.hasNext()) {
+        // grab the next word, make it lowercase, filter it, then increment its
+        // value in the HashMap frequency
+        incrementCounter(sc.next().toLowerCase().replaceAll(filter, ""));
+      }
+
+      // close the Scanner
+      sc.close();
+    }
+    // the supplied file was not found
+    catch (FileNotFoundException e) {
+      // report to user what went wrong
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Sorts the entries in the HashMap from greatest to smallest and writes them
+   * all to a .txt file
+   *
+   * @param outputFile The filer we wish to write our result out to
+   */
+  public void exportWords(String outputFile) {
+    // create a list of keys
+    List<String> keyList = new ArrayList<>();
+
+    // populate keyList with all keys from HashMap frequency
+    for (String key : frequency.keySet()) {
+      keyList.add(key);
+    }
+
+    // sorts the keyList from highest to lowest based on the value associated with each key
+    Collections.sort(keyList, new Comparator<String>() {
+      // Sort from hightest to lowest
+      public int compare(String s1, String s2) {
+        return frequency.get(s2) - frequency.get(s1);
+      }
+    });
+
+    try {
+      // create FileWriter to create a txt file filled with word/frequency pairs
+      FileWriter fw = new FileWriter(new File(outputFile));
+
+      // iterate over the sorted list writing each one out to out outputFile
+      for (String key : keyList) {
+        // write to outputFile in the style of 'frequency: word'
+        fw.write(frequency.get(key) + ": " + key + "\n");
+      }
+
+      // close FileWriter
+      fw.close();
+    }
+    catch (IOException e) {
+      // if anything goes wrong, report the exception to the user
+      e.printStackTrace();
+    }
   }
 }
